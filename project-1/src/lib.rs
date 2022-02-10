f4b43ae7289a6d3b100a04ed41dde6892314dcba
sd <sd@jooble.com>
2022-02-10 02:49:52 +0200

    add docs for the project-1

--- project-1/src/lib.rs
+++ project-1/src/lib.rs
@@ -5,19 +5,33 @@ pub struct KvStore {
     storage: HashMap<String, String>
 }
 
+/// In memory key value storage String:String
+/// Usage
+/// ```rust
+/// # use std::error::Error;
+/// #
+/// # fn main() -> Result<(), Box<dyn Error>> {
+/// let mut store = KvStore::new();
+/// store.set("key1".to_owned(), "value1".to_owned());
+/// store.get("key2".to_owned());
+/// store.remove("key1".to_owned());
+/// #
+/// #     Ok(())
+/// # }
+/// ```
 impl KvStore {
     pub fn new() -> Self {
         Self {storage: HashMap::new()}
     }
-
+    /// Set up value by key into KVS
     pub fn set(&mut self, key: String, value: String) {
         self.storage.insert(key, value);
     }
-
+    /// Get value by key
     pub fn get(&self, key: String) -> Option<String> {
         self.storage.get(key.as_str()).cloned()
     }
-
+    /// Removes value by key
     pub fn remove(&mut self, key: String) {
         self.storage.remove(key.as_str());
     }
