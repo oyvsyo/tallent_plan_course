7bdaee456ccc4518281bfdb8d40c02c3ed1cb3f9
sd <sd@jooble.com>
2022-04-12 13:27:42 +0300

    restructure engine backends

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
 pub use engine::KvsEngine;
 pub use error::{KVSError, Result};
-pub use kv_store::KvStore;
-pub use sled_store::SledStore;
+pub use storages::kv_store::KvStore;
+pub use storages::sled_store::SledStore;
 pub use tcp::client::KVSClient;
 pub use tcp::protocol::{DBCommands, ServerResponse};
 pub use tcp::server::KvsServer;
 
 mod engine;
 mod error;
-mod kv_store;
-mod sled_store;
+mod storages {
+    pub mod kv_store;
+    pub mod sled_store;
+}
 mod tcp {
     pub mod client;
     pub mod protocol;
