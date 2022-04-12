5fc5114f4d41b485a1eef463b88a4008622f68cf
sd <sd@jooble.com>
2022-04-13 02:11:53 +0300

    fmt + clippy

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,25 +1,23 @@
 #[macro_use]
 extern crate criterion;
-use std::iter::{zip};
-use rand::distributions::{Alphanumeric, DistString};
-use rand::Rng;
 use criterion::BenchmarkId;
 use criterion::Criterion;
+use rand::distributions::{Alphanumeric, DistString};
+use rand::Rng;
+use std::iter::zip;
 
-use kvs::{KvStore, SledStore, KvsEngine, Result};
+use kvs::{KvStore, KvsEngine, Result, SledStore};
 use tempfile::TempDir;
 
-
-fn generate_strings(num: usize, min: usize, max: usize) -> Result<Vec<String>>{
-    
+fn generate_strings(num: usize, min: usize, max: usize) -> Result<Vec<String>> {
     let mut strings = vec![];
     let mut rng = rand::thread_rng();
-    
+
     for _ in 0..num {
         let len: usize = rng.gen_range(min..max);
         let string = Alphanumeric.sample_string(&mut rand::thread_rng(), len);
         strings.push(string);
-    } 
+    }
     Ok(strings)
 }
 
@@ -41,53 +39,69 @@ fn get_values<S: KvsEngine>(storage: &mut S, keys: Vec<String>, values: Vec<Stri
 }
 
 fn set_benchmark(c: &mut Criterion) {
-
     let mut group = c.benchmark_group("set_benchmark");
 
     for store_type in ["kvs", "sled"].iter() {
-        group.bench_with_input(BenchmarkId::new("input_example", store_type), &store_type, |b, &s| {
-            let keys = generate_strings(100, 1, 100000).expect("Cant create strings");
-            let values = generate_strings(100, 1, 100000).expect("Cant create strings");
-            let temp_dir = TempDir::new().unwrap();
-            match *s{
-                "kvs" => {
-                    let mut store = KvStore::open(temp_dir.path()).expect("cant create store");
-                    b.iter(|| set_values(&mut store, copy_strings(&keys), copy_strings(&values)));
+        group.bench_with_input(
+            BenchmarkId::new("input_example", store_type),
+            &store_type,
+            |b, &s| {
+                let keys = generate_strings(100, 1, 100000).expect("Cant create strings");
+                let values = generate_strings(100, 1, 100000).expect("Cant create strings");
+                let temp_dir = TempDir::new().unwrap();
+                match *s {
+                    "kvs" => {
+                        let mut store = KvStore::open(temp_dir.path()).expect("cant create store");
+                        b.iter(|| {
+                            set_values(&mut store, copy_strings(&keys), copy_strings(&values))
+                        });
+                    }
+                    "sled" => {
+                        let mut store =
+                            SledStore::open(temp_dir.path()).expect("cant create store");
+                        b.iter(|| {
+                            set_values(&mut store, copy_strings(&keys), copy_strings(&values))
+                        })
+                    }
+                    _ => (),
                 }
-                "sled" => {
-                    let mut store = SledStore::open(temp_dir.path()).expect("cant create store");
-                    b.iter(|| set_values(&mut store, copy_strings(&keys), copy_strings(&values)))
-                }
-                _ => ()
-            }
-        });
+            },
+        );
     }
     group.finish();
 }
 
 fn get_benchmark(c: &mut Criterion) {
-
     let mut group = c.benchmark_group("get_benchmark");
 
     for store_type in ["kvs", "sled"].iter() {
-        group.bench_with_input(BenchmarkId::new("input_example", store_type), &store_type, |b, &s| {
-            let keys = generate_strings(100, 1, 100000).expect("Cant create strings");
-            let values = generate_strings(100, 1, 100000).expect("Cant create strings");
-            let temp_dir = TempDir::new().unwrap();
-            match *s{
-                "kvs" => {
-                    let mut store = KvStore::open(temp_dir.path()).expect("cant create store");
-                    set_values(&mut store, copy_strings(&keys), copy_strings(&values));
-                    b.iter(|| get_values(&mut store, copy_strings(&keys), copy_strings(&values)));
+        group.bench_with_input(
+            BenchmarkId::new("input_example", store_type),
+            &store_type,
+            |b, &s| {
+                let keys = generate_strings(100, 1, 100000).expect("Cant create strings");
+                let values = generate_strings(100, 1, 100000).expect("Cant create strings");
+                let temp_dir = TempDir::new().unwrap();
+                match *s {
+                    "kvs" => {
+                        let mut store = KvStore::open(temp_dir.path()).expect("cant create store");
+                        set_values(&mut store, copy_strings(&keys), copy_strings(&values));
+                        b.iter(|| {
+                            get_values(&mut store, copy_strings(&keys), copy_strings(&values))
+                        });
+                    }
+                    "sled" => {
+                        let mut store =
+                            SledStore::open(temp_dir.path()).expect("cant create store");
+                        set_values(&mut store, copy_strings(&keys), copy_strings(&values));
+                        b.iter(|| {
+                            get_values(&mut store, copy_strings(&keys), copy_strings(&values))
+                        })
+                    }
+                    _ => (),
                 }
-                "sled" => {
-                    let mut store = SledStore::open(temp_dir.path()).expect("cant create store");
-                    set_values(&mut store, copy_strings(&keys), copy_strings(&values));
-                    b.iter(|| get_values(&mut store, copy_strings(&keys), copy_strings(&values)))
-                }
-                _ => ()
-            }
-        });
+            },
+        );
     }
     group.finish();
 }
