9a04cb3b537725f7957dbbdedb28dcf7a52451cb
oyvsyo <dvoiak.stepan@gmail.com>
2022-04-20 15:55:08 +0000

    Deploying to docs from @ oyvsyo/tallent_plan_course@c258cc0088c2676877b621b7a9a7ca0fb06803d7 🚀

--- docs/implementors/std/error/trait.Error.js
+++ docs/implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["kvs"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.59.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"kvs/enum.KVSError.html\" title=\"enum kvs::KVSError\">KVSError</a>","synthetic":false,"types":["kvs::error::KVSError"]}];
+implementors["kvs"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"kvs/enum.KVSError.html\" title=\"enum kvs::KVSError\">KVSError</a>","synthetic":false,"types":["kvs::error::KVSError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
